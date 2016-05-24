f00b467abca00e7e45835aee3fdbd4f82aaa2efa
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-05-24 15:43:58 +0300

    LCO: ServiceProvider initial.

---
+++ src/ServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DmitryIvanov\LaravelConsoleOverlapping;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
