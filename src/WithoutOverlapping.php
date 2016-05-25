99527e2f1155cb2f5a4dcf1b8b04f5d60958d543
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-05-25 15:13:30 +0300

    LCO: Execute method overriden.

--- src/WithoutOverlapping.php
+++ src/WithoutOverlapping.php
@@ -2,6 +2,15 @@
 
 namespace Illuminated\Console;
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 trait WithoutOverlapping
 {
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        var_dump('overlapping check here');
+
+        return parent::execute($input, $output);
+    }
 }
