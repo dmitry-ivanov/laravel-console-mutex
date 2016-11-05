f551e45cba23bb945fe644e00cae41ad4533992b
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-05 21:49:18 +0200

    ICM: Console kernel overloaded.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,11 @@
 <?php
 
+use Illuminate\Contracts\Console\Kernel as KernelContract;
+
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
+    protected function resolveApplicationConsoleKernel($app)
+    {
+        $app->singleton(KernelContract::class, Kernel::class);
+    }
 }
