47955942567c72735e241cc2a7330da2ef27d200
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-05 21:49:36 +0200

    ICM: Dummy test added.

--- tests/DummyTest.php
+++ tests/DummyTest.php
@@ -5,6 +5,10 @@ class DummyTest extends TestCase
     /** @test */
     public function it_does_nothing()
     {
-        $this->assertTrue(true);
+        $code = Artisan::call('foo');
+        $output = Artisan::output();
+
+        $this->assertEquals(0, $code);
+        $this->assertContains('Yep!', $output);
     }
 }
