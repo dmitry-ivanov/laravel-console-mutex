486adbc3afbf56c7a295d4f6a904eff54e222510
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-07 18:06:43 +0200

    ICM: Tests added.

--- tests/WithoutOverlappingTraitTest.php
+++ tests/WithoutOverlappingTraitTest.php
@@ -3,16 +3,24 @@
 class WithoutOverlappingTraitTest extends TestCase
 {
     /** @test */
-    public function it_adds_mutex_strategy_property_which_is_file_by_default()
+    public function it_adds_mutex_strategy_concept_which_is_file_by_default()
     {
         $command = new GenericCommand;
         $this->assertEquals('file', $command->getMutexStrategy());
     }
 
     /** @test */
-    public function mutex_strategy_can_by_overloaded_by_protected_field()
+    public function mutex_strategy_can_be_overloaded_by_protected_field()
     {
         $command = new MysqlStrategyCommand;
         $this->assertEquals('mysql', $command->getMutexStrategy());
+    }
+
+    /** @test */
+    public function mutex_strategy_can_be_set_by_the_public_method()
+    {
+        $command = new GenericCommand;
+        $command->setMutexStrategy('redis');
+        $this->assertEquals('redis', $command->getMutexStrategy());
     }
 }
