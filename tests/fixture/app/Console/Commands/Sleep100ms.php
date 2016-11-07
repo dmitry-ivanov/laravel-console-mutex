47c36c15dba82f2140c50771eac8e414b0dcbc88
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-07 15:56:30 +0200

    ICM: Trait used.

--- tests/fixture/app/Console/Commands/Sleep100ms.php
+++ tests/fixture/app/Console/Commands/Sleep100ms.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Console\Command;
+use Illuminated\Console\WithoutOverlapping;
 
 class Sleep100ms extends Command
 {
+    use WithoutOverlapping;
+
     protected $signature = 'sleep-100ms';
 
     public function handle()
