efee88909d6beb0dee09586afd109c1462ef2fca
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-09 14:33:05 +0200

    ICM: Test fixed.

--- tests/WithoutOverlappingTraitTest.php
+++ tests/WithoutOverlappingTraitTest.php
@@ -40,6 +40,20 @@ class WithoutOverlappingTraitTest extends TestCase
      * @runInSeparateProcess
      * @preserveGlobalState disabled
      */
+    public function it_allows_to_run_command_if_there_is_no_other_running_instances()
+    {
+        $mutex = Mockery::mock('overload:Illuminated\Console\Mutex');
+        $mutex->shouldReceive('acquireLock')->with(0)->once()->andReturn(true);
+        $mutex->shouldReceive('releaseLock')->withNoArgs();
+
+        Artisan::call('icm:generic');
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
     public function it_throws_an_exception_if_trying_to_run_another_instance_of_the_command()
     {
         $mutex = Mockery::mock('overload:Illuminated\Console\Mutex');
@@ -63,24 +77,5 @@ class WithoutOverlappingTraitTest extends TestCase
     //     $mutex->shouldReceive('releaseLock')->withNoArgs();
     //
     //     Artisan::call('icm:generic');
-    // }
-    //
-    // /**
-    //  * @test
-    //  * @runInSeparateProcess
-    //  * @preserveGlobalState disabled
-    //  */
-    // public function it_stops_execution_if_lock_had_not_been_acquired()
-    // {
-    //     $mutex = Mockery::mock('overload:Illuminated\Console\Mutex');
-    //     $mutex->shouldReceive('acquireLock')->with(0)->once()->andReturn(false);
-    //     $mutex->shouldReceive('releaseLock')->withNoArgs();
-    //     Mockery::mock()->shouldReceive('exit')->withNoArgs()->once();
-    //
-    //     $code = Artisan::call('icm:generic');
-    //     $output = Artisan::getOutput();
-    //
-    //     $this->assertEquals(0, $code);
-    //     $this->assertContains('Command is running now!', $output);
     // }
 }
