0ba3224507dec3cc94722945d8f0c00cea60cca8
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-09 18:49:34 +0200

    ICM: Tests added.

--- tests/MutexTest.php
+++ tests/MutexTest.php
@@ -1,26 +1,40 @@
 <?php
 
 use Illuminated\Console\Mutex;
+use NinjaMutex\Lock\FlockLock;
 
 class MutexTest extends TestCase
 {
+    private $command;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->command = Mockery::mock(GenericCommand::class)->makePartial();
+        $this->command->shouldReceive('argument')->withNoArgs()->once()->andReturn(['foo' => 'bar']);
+    }
+
     /** @test */
     public function it_has_one_required_constructor_argument_which_is_command()
     {
-        $command = Mockery::mock(GenericCommand::class)->makePartial();
-        $command->shouldReceive('argument')->withNoArgs()->once()->andReturn(['foo' => 'bar']);
-
-        $mutex = new Mutex($command);
+        $mutex = new Mutex($this->command);
         $this->assertInstanceOf(Mutex::class, $mutex);
     }
 
     /** @test */
     public function it_determines_mutex_strategy_once_while_creation()
     {
-        $command = Mockery::mock(GenericCommand::class)->makePartial();
-        $command->shouldReceive('argument')->withNoArgs()->once()->andReturn(['foo' => 'bar']);
-
-        $mutex = new Mutex($command);
+        $mutex = new Mutex($this->command);
         $this->assertSame($mutex->getStrategy(), $mutex->getStrategy());
+    }
+
+    /** @test */
+    public function it_has_default_strategy_which_is_file()
+    {
+        $this->command->shouldReceive('getMutexStrategy')->withNoArgs()->once()->andReturn('foobar');
+
+        $mutex = new Mutex($this->command);
+        $this->assertEquals(new FlockLock(storage_path('app')), $mutex->getStrategy());
     }
 }
