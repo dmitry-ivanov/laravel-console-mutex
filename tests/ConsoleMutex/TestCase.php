4ae99e1be67ce1091e03717c9b2131e90cbbf4b7
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-03-09 12:00:56 +0200

    ICM: Testing tools used.

--- tests/ConsoleMutex/TestCase.php
+++ tests/ConsoleMutex/TestCase.php
@@ -3,10 +3,13 @@
 namespace Illuminated\Console\ConsoleMutex\Tests;
 
 use Illuminate\Contracts\Console\Kernel as KernelContract;
+use Illuminated\Testing\TestingTools;
 use Kernel;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
+    use TestingTools;
+
     protected function resolveApplicationConsoleKernel($app)
     {
         $app->singleton(KernelContract::class, Kernel::class);
